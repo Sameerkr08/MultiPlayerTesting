602672c5f2a1d649ba9e4183b4b1c84aca872927
Sameer Kumar <sssameersingh08041997@gmail.com>
2025-10-19 20:29:56 +0530

    Bot system and Animation Sync

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using Photon.Pun;
 
-public class EnemyAi : MonoBehaviour
+public class EnemyAi : MonoBehaviourPunCallbacks
 {
     NavMeshAgent _agent;
     [SerializeField] Image _healthBar;
@@ -21,34 +22,49 @@ public class EnemyAi : MonoBehaviour
 
     void Update()
     {
-        if (_trigger && Vector3.Distance(transform.position, _target.transform.position) > _agent.stoppingDistance)
+        if (_target != null)
         {
-            _animator.SetBool("Attack", false);
-            _animator.SetBool("Run", true);
-            _agent.SetDestination(_target.transform.position);
-        }
-        else if (Vector3.Distance(transform.position, _target.transform.position) <= _agent.stoppingDistance)
-        {
-            Attack();
-            _agent.velocity = Vector3.zero;
-        }
-        else if(Vector3.Distance(transform.position, _target.transform.position) <= _triggerRange || _trigger)
-        {
-            _animator.SetBool("Attack", false);
-            _animator.SetBool("Run", true);
-            _agent.SetDestination(_target.transform.position);
+            if (_trigger && Vector3.Distance(transform.position, _target.transform.position) > _agent.stoppingDistance)
+            {
+                _animator.SetBool("Attack", false);
+                _animator.SetBool("Run", true);
+                _agent.SetDestination(_target.transform.position);
+            }
+            else if (Vector3.Distance(transform.position, _target.transform.position) <= _agent.stoppingDistance)
+            {
+                Attack();
+                _agent.velocity = Vector3.zero;
+            }
+            else if (Vector3.Distance(transform.position, _target.transform.position) <= _triggerRange || _trigger)
+            {
+                _animator.SetBool("Attack", false);
+                _animator.SetBool("Run", true);
+                _agent.SetDestination(_target.transform.position);
+            }
+            else
+            {
+                _agent.velocity = Vector3.zero;
+                _animator.SetBool("Attack", false);
+                _animator.SetBool("Run", false);
+                _target = null;
+            }
         }
         else
         {
-            _agent.velocity = Vector3.zero;
-            _animator.SetBool("Attack", false);
-            _animator.SetBool("Run", false);
+            foreach (PlayerMovement _player in FindObjectsByType<PlayerMovement>(FindObjectsSortMode.None))
+            {
+                if(Vector3.Distance(transform.position, _player.gameObject.transform.position) <= _triggerRange)
+                {
+                    _target = _player.gameObject;
+                }
+            }
         }
         _healthBar.fillAmount = _health / _maxhealth;
     }
 
-    public void GotHit(GameObject game)
+    public void GotHit(GameObject _player)
     {
+        _target = _player;
         _health -= 20;
         _trigger = true;
     }
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -3,12 +3,16 @@ using Photon.Pun;
 
 public class UiManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] GameObject  _player;
+    [SerializeField] GameObject  _player, _enemy;
     GameObject _cube;
 
     private void Start()
     {
         PhotonNetwork.Instantiate(_player.name, _player.transform.position, Quaternion.identity);
+        if (PhotonNetwork.LocalPlayer == PhotonNetwork.MasterClient)
+        {
+            PhotonNetwork.InstantiateRoomObject(_enemy.name, _enemy.transform.position, Quaternion.identity);
+        }
     }
 
     private void Update()
